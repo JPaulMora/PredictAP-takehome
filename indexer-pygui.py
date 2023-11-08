7d3d5afc7d78a5839f2a9be7ca0d563a567fe308
Juan Mora <juan@nimble.gt>
2023-11-08 01:58:05 -0600

    Complete solution

--- indexer-pygui.py
+++ indexer-pygui.py
@@ -18,7 +18,7 @@ layout = [
     ],
     [sg.Text("", key="index_counter"), sg.Button("Test button", key="test_btn")],
     [sg.Input("", key="search_input", visible=False), sg.Combo(['Any'], key="extension_combo", visible=False, size=10)],
-    [sg.Listbox(values=[], key="file_list", size=(60, 6), visible=False,)],
+    [sg.Listbox(values=[], key="file_list", size=(70, 6), visible=False,)],
     [sg.Button("Search", key="search_btn", visible=False)]
 ]
 window = sg.Window("Indexer", layout, resizable=True)
@@ -26,18 +26,18 @@ window = sg.Window("Indexer", layout, resizable=True)
 def index_folder(folder):
     db = sqlite3.connect(folder + "/index.sqlite3")
     db.execute(
-        "CREATE TABLE IF NOT EXISTS FileIndex (id INTEGER PRIMARY KEY, file_name TEXT, size_bytes INTEGER, content_type TEXT ALLOW NULL)"
+        "CREATE TABLE IF NOT EXISTS FileIndex (id INTEGER PRIMARY KEY, absolute_path TEXT UNIQUE, file_name TEXT, size_bytes INTEGER, content_type TEXT ALLOW NULL)"
     )
 
     records = []
     file_count = 0
     for file in Path(folder).rglob("*"):
-        records.append((file.name, file.stat().st_size, file.suffix))
+        records.append((str(file.resolve()), file.name, file.stat().st_size, file.suffix))
         file_count += 1
         window.write_event_value("indexer_msg", file_count)
 
     db.executemany(
-        "INSERT INTO FileIndex(file_name, size_bytes, content_type) VALUES(?, ?, ?)",
+        "INSERT OR IGNORE INTO FileIndex(absolute_path ,file_name, size_bytes, content_type) VALUES(?, ?, ?, ?)",
         records,
     )
     db.commit()
@@ -51,6 +51,27 @@ def get_available_file_extensions(folder):
     extensions.append('Any')
     return extensions
 
+def human_readable_filesize(size):
+    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti']:
+        if abs(size) < 1024.0:
+            return f"{size:.1f}{unit}B"
+        size /= 1024.0
+
+
+def search_files(folder, name, file_ext=None):
+    sql = "SELECT file_name, size_bytes FROM FileIndex WHERE file_name LIKE ?"
+    query_params = ("%" + name + "%",)
+    if file_ext not in ["Any", None, ""]:
+        sql += " AND content_type = ?"
+        query_params += (file_ext,)
+    
+    db = sqlite3.connect(folder + "/index.sqlite3")
+    files = []
+    for file in db.execute(sql, query_params):
+        files.append(f"{file[0]} - {human_readable_filesize(file[1])}")
+    db.close()
+    return files
+
 
 while True:
     event, values = window.read()
@@ -60,14 +81,12 @@ while True:
         folder = values["-FOLDER-"]
         if Path(folder + "/index.sqlite3").is_file():
             window["reindex_btn"].Update(visible=True)
-            window["extension_combo"].Update(values=get_available_file_extensions(folder))
+            window["search_input"].Update(visible=True)
+            window["extension_combo"].Update(visible=True, values=get_available_file_extensions(folder))
+            window["file_list"].Update(visible=True)
+            window["search_btn"].Update(visible=True)
         else:
             window["index_btn"].Update(visible=True)
-
-        window["extension_combo"].Update(visible=True)
-        window["search_input"].Update(visible=True)
-        window["file_list"].Update(visible=True)
-        window["search_btn"].Update(visible=True)
 
     if event == "index_btn" or event == "reindex_btn":
         window["index_btn"].Update(disabled=True)
@@ -75,9 +94,11 @@ while True:
 
     if event == "indexer_msg":
         window["index_counter"].Update(f"Indexed {values['indexer_msg']} files...")
+
+    if event == "search_btn":
+        window["file_list"].Update(values=search_files(folder, values['search_input'], values['extension_combo']))
     
     if event == "test_btn":
-        # get_available_file_extensions()
         pass
 
     elif event == "index_completed":
